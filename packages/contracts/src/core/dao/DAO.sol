08c0ff63306ddd31473c2b843c205c3fdd29fea7
Mauricio Zanetti Filho <contact@thinkincoin.com>
2023-11-26 19:12:26 -0300

    test

--- packages/contracts/src/core/dao/DAO.sol
+++ packages/contracts/src/core/dao/DAO.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.8;
 
-import "https://raw.githubusercontent.com/mzfshark/contracts-upgradeable/v4.9.3/contracts/utils/introspection/ERC165StorageUpgradeable.sol";
+import "../../utils/introspection/ERC165StorageUpgradeable.sol";
 import "https://raw.githubusercontent.com/mzfshark/contracts-upgradeable/v4.9.3/contracts/proxy/utils/Initializable.sol";
 import "https://raw.githubusercontent.com/mzfshark/contracts-upgradeable/v4.9.3/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "https://raw.githubusercontent.com/mzfshark/contracts-upgradeable/v4.9.3/contracts/token/ERC20/utils/SafeERC20Upgradeable.sol";
