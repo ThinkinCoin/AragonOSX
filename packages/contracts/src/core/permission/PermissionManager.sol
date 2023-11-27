10c88d7ab2edf32f0790f17faba1e1700f9d481a
Mauricio Zanetti Filho <contact@thinkincoin.com>
2023-11-26 21:16:39 -0300

    fix

--- packages/contracts/src/core/permission/PermissionManager.sol
+++ packages/contracts/src/core/permission/PermissionManager.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.8;
 
-import "../../../proxy/utils/initializable.sol";
+import "../../proxy/utils/initializable.sol";
 import "../../../utils/AddressUpgradeable.sol";
 
 import {IPermissionCondition} from "./IPermissionCondition.sol";
