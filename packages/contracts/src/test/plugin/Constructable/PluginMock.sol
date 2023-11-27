d8e942d17cc478ec56529f91bdd5d565ad3958b0
Mauricio Zanetti Filho <contact@thinkincoin.com>
2023-11-27 08:55:56 -0300

    fix

--- packages/contracts/src/test/plugin/Constructable/PluginMock.sol
+++ packages/contracts/src/test/plugin/Constructable/PluginMock.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.8;
 
-import {Plugin} from "../../core/plugin/Plugin.sol";
-import {IDAO} from "../../core/dao/IDAO.sol";
+import {Plugin} from "../../../core/plugin/Plugin.sol";
+import {IDAO} from "../../../core/dao/IDAO.sol";
 
 contract PluginV1Mock is Plugin {
     uint256 public state1;
